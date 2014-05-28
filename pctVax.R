1d6bc22087dd0e340abcc540aa8cf4972b9136a6
Juliet Pulliam <pulliam@EPI-C02G90Q1DJ.local>
2014-05-28 11:30:32 +0200

    proportion needed to vaccinate
    jp fig for sb to modify

---
+++ pctVax.R
@@ -0,0 +1,26 @@
+## pctVax.R
+## JRCP 25 March 2014
+##
+## Unit 6 - Applied Ecology of Infectious Diseases
+## ZOO4926/ZOO6927 Spring 2014, Department of Biology
+## University of Florida, Gainesville, Florida, USA
+
+# Disease list
+# R0 estimates from Anderson & May 1982 Science:
+dl <- list(measles=c(13.7,12.5,18),
+	pertussis=c(14.3,17.1,12.2),
+	chickenpox = c(8.5,9),
+	diptheria=c(6.6,6.4),
+	mumps=c(4.3,7.1),
+	rubella=c(6,6.7),
+	poliomyelitis=c(5.9,6.2))
+
+# Percent needed to vaccinate plot
+par(mfcol=c(1,1),mar=c(5,6,2,1),bty="L",lwd=2)
+curve(1-1/x,xlab=expression(R[0]),ylab=expression(p[v]),ylim=c(0,1),xlim=c(0,20),lwd=6)
+sapply(1:length(dl),function(dis){
+	xx <- dl[[dis]]
+	yy <- 1-1/dl[[dis]]
+	points(xx,yy,pch=16,cex=1.8,col=dis+1)
+	text(15,dis/10,labels=names(dl)[dis],col=dis+1,pos=2,cex=2)
+})
